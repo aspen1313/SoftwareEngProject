0944dca7274bfdd8ef6769550cbceef0477f6af8
Hibah <hibah@github.com>
2019-02-17 15:57:15 -0400

    Created UI for the open date
    Createed the UI for setting an open date - HA

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void buttonOnClick(View v) {
+        Button button = (Button)v;
+        startActivity(new Intent(getApplicationContext(), Main2Activity.class));
     }
 }
