4b83354a3bab33cfcc416a89c4e61fce78081463
Liam <liamhartery@gmail.com>
2019-03-25 11:04:50 -0300

    Added Tests for the user class as well as more methods in IUser
    LH / AA

--- app/src/main/java/com/example/project/models/IUser.java
+++ app/src/main/java/com/example/project/models/IUser.java
@@ -5,12 +5,18 @@ package com.example.project.models;
  */
 public interface IUser {
 
+    /** Returns true if the User is an admin.*/
     boolean isAdmin();
 
+    /** Returns true if the User has voted on poll with ID pollId*/
     boolean hasVotedOnPoll(String pollId);
 
+    /** Returns the User's username.*/
     String getUsername();
 
+    /** Returns the ID that FireStore gave this object. */
     String getFirestoreId();
 
+    /** Tells the User that it voted on a specific poll*/
+    void votedOnPoll(String pollId);
 }
---
+++ app/src/test/java/com/example/project/UserTests.java
@@ -0,0 +1,69 @@
+package com.example.project;
+
+import com.example.project.models.IUser;
+import com.example.project.models.Poll;
+import com.example.project.models.Question;
+import com.example.project.models.User;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+public class UserTests {
+    private IUser user;
+    private IUser adminUser;
+    private Poll poll;
+    private Poll notVotedPoll;
+    private String VOTED_ON_POLL_ID = "VOTED_POLL";
+    private String NOT_VOTED_POLL_ID = "NOT_VOTED";
+    private String USERNAME = "username";
+
+    @Before
+    public void runBeforeTests(){
+        ArrayList<String> options = new ArrayList<>();
+        options.add("Option 1");
+        options.add("Option 2");
+        Question question = Question.getNewQuestion("Test Question", options);
+        poll = Poll.getNewElection("Test Poll", question);
+        poll.id = VOTED_ON_POLL_ID;
+
+        notVotedPoll = Poll.getNewElection("Test Poll 2", question);
+        notVotedPoll.id = NOT_VOTED_POLL_ID;
+
+        user = new User(USERNAME, false);
+        user.votedOnPoll(poll.id);
+
+        adminUser = new User(USERNAME, true);
+    }
+
+    @Test
+    public void hasVotedOnPollReturnsFalseForPollsNotVotedOn(){
+        assertFalse(user.hasVotedOnPoll(NOT_VOTED_POLL_ID));
+    }
+
+    @Test
+    public void hasVotedOnPollReturnsTrueForPollsVotedOn(){
+        assertTrue(user.hasVotedOnPoll(VOTED_ON_POLL_ID));
+    }
+
+    @Test
+    public void getUsernameReturnsCorrectUsername(){
+        assertEquals(USERNAME, user.getUsername());
+    }
+
+    @Test
+    public void isAdminReturnsTrueWhenUserIsAdmin(){
+        assertTrue(adminUser.isAdmin());
+    }
+
+    @Test
+    public void isAdminReturnsFalseWhenUserNotAdmin(){
+        assertFalse(user.isAdmin());
+    }
+}
