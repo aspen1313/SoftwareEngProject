58511cd60e9ccae2e90dd152491b78cceb6a73cf
Liam <liamhartery@gmail.com>
2019-03-25 12:39:14 -0300

    Added implementation for User class
    LH / AA

--- app/src/test/java/com/example/project/UserTests.java
+++ app/src/test/java/com/example/project/UserTests.java
@@ -13,7 +13,6 @@ import java.util.ArrayList;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 public class UserTests {
     private IUser user;
