d61e0ba72b5f5d0869bae13c35a1445f96e0f0d4
Liam <liamhartery@gmail.com>
2019-03-29 17:20:17 -0300

    Changed tests for UserManager and some of the stubs
    We realized that we cannot access the firestore from a java class in this way, so we will require an activity before we can access it.

--- app/src/main/java/com/example/project/models/UserManager.java
+++ app/src/main/java/com/example/project/models/UserManager.java
@@ -1,12 +1,9 @@
 package com.example.project.models;
 
-import com.google.firebase.firestore.FirebaseFirestore;
-
 public class UserManager {
     private static User user;
 
-    public static void login(String username){
-        // Get user with username from firebase and set the user variable.
+    public static void login(User user){
     }
 
     public static void logout(){
@@ -15,9 +12,5 @@ public class UserManager {
 
     public static User getUser(){
         return user;
-    }
-
-    public static User registerUser(String username, boolean isAdmin){
-        return null;
     }
 }
--- app/src/test/java/com/example/project/UserManagerTests.java
+++ app/src/test/java/com/example/project/UserManagerTests.java
@@ -3,35 +3,40 @@ package com.example.project;
 import com.example.project.models.User;
 import com.example.project.models.UserManager;
 
+import org.junit.Before;
 import org.junit.Test;
 
-import static junit.framework.TestCase.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
 
 public class UserManagerTests {
     private static final String USERNAME = "dummyUsername";
+    private static final String FIRESTORE_ID = "12345QWERTY";
+    private User dummyUser;
 
+    @Before
+    public void runBefore(){
+        dummyUser = new User(FIRESTORE_ID, USERNAME, true);
+    }
+
+    /**
+     * Ensures that the user can be logged in correctly
+     */
     @Test
     public void loginCorrectlyAssignsUser(){
-        UserManager.login("dummyUsername");
+        UserManager.login(dummyUser);
         User u = UserManager.getUser();
         assertNotNull(u);
     }
 
+    /**
+     * Logout requires login to be working, so we test that first.
+     */
     @Test
     public void logoutCorrectlyVoidsUser(){
-        UserManager.login("dummyUsername");
+        UserManager.login(dummyUser);
         assertNotNull(UserManager.getUser());
         UserManager.logout();
         assertNull(UserManager.getUser());
-    }
-
-    @Test
-    public void registerUserCorrectlyCreatesUserObject(){
-        User u = UserManager.registerUser(USERNAME, true);
-        assertEquals(USERNAME, u.getUsername());
-        assertTrue(u.isAdmin());
     }
 }
